56d4e0150fa4ee962372d8451d61a91e5c4be0d2
李天广 <litianguang@echiele.com>
2018-06-07 15:27:52 +0800

    初始化

---
+++ Test002/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test002
+//
+//  Created by Andy Li on 2018/6/7.
+//  Copyright © 2018年 AndyLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test002/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test002
+//
+//  Created by Andy Li on 2018/6/7.
+//  Copyright © 2018年 AndyLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
